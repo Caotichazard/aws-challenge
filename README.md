adef7e244f55cc9984c5ebcc9e6b0ed3751f6de6
CaotiHazard <guilsalomao@gmail.com>
2021-07-16 11:42:04 -0300

    formatada no Readme

--- README.md
+++ README.md
@@ -1,35 +1,54 @@
 # aws-challenge
 AWS Challenge for Instituto de Pesquisas Eldorado
 
+[The challenge](https://github.com/Caotichazard/aws-challenge/blob/main/AWS%20Challenge.pdf)
+
+Make a simple device management web app where you can create a category and add devices with a category.
+
+technologies used:
+- Angular and AngularCLI version 12.1.1
+- Node ver 16.4.2
+- MySQL 15.1 with MariaDB 10.5.11
+
 
 ## Setup
 
 First, clone the repo, then inside the backend folder execute first
+
     npm install
 
 then, setup the DB by initializing the db
+
     sudo systemctl start mysqld
 
-then with
-    sudo mysql -u root
+then access mysql as root with
 
-use 
+    sudo mysql -u root -p
+
+and use the init.sql script to create the database, tables and user needed
+
     source src/api/init.sql
 
-this will create the user and add the db and tables
+
 
 then, change to frontend folder and go into web-app folder and execute
+
     npm install --legacy-peer-deps
+
+with this, you enviroment will be configured and ready to use.
 
 
 
 ## Running
 
-First, inside the backend folder initialize mysql with 
+First, initialize mysql with 
+
     sudo systemctl start mysqld
 
-then open the backend server with 
+then, navigate to the backend directory and open the backend server with 
+
     node src/api/server.js
 
-after that, go back to frontend folder and run
+after that, go back to frontend directory and run
+
     ng serve --open
--- frontend/web-app/src/app/device.service.ts
+++ frontend/web-app/src/app/device.service.ts
@@ -3,7 +3,7 @@ import { Device } from './device';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
-//serviço injetável para usar de interface com a api de categorias
+//serviço injetável para usar de interface com a api de dispositivos
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,12 @@ import { MessageService } from './message.service';
 
 
 export class DeviceService {
-  private devicesUrl = 'http://localhost:3000/devices';//url para obter as categorias
+  private devicesUrl = 'http://localhost:3000/devices';//url para obter as dispositivos
   
   //Construtor, recebe tanto o cliente para realizar as operações com a API quanto o Serviço de Mensagens
   constructor(private http: HttpClient,private messageService: MessageService) { }
 
-  //headers para poder realizar a operação de adicionar uma categoria
+  //headers para poder realizar a operação de adicionar uma dispositivo
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -26,24 +26,24 @@ export class DeviceService {
     this.messageService.add(`DeviceServices: ${message}`);
   }
 
-  //obtem todas as categorias da API
+  //obtem todas as dispositivos da API
   //Observable configura ele como uma operação assincrona, nesse caso ele envia a mensagem dizendo que está
-  //recebendo as categorias e envia o pedido para a api, isso imediatamente retorna um observable que, para ser usado
+  //recebendo as dispositivos e envia o pedido para a api, isso imediatamente retorna um observable que, para ser usado
   //dever ser "subscribed", e quando o mesmo tiver o retorno da API, será atualizado.
   getDevices(): Observable<Device[]> {
     this.log("fetching devices")
     return this.http.get<Device[]>(this.devicesUrl);
   }
 
-  //dado o ID de uma categoria, deleta a mesma
-  //usando o url base, da um append para colocar como parametro o id da categoria
+  //dado o ID de uma dispositivo, deleta a mesma
+  //usando o url base, da um append para colocar como parametro o id da dispositivo
   deleteDevice(id:number): Observable<Device>{
     const url = `${this.devicesUrl}/${id}`;
     this.log("deleting device id:" + id );
     return this.http.delete<Device>(url);
   }
 
-  //cria uma categoria nova
+  //cria uma dispositivo nova
   addDevice(device:Device):Observable<Device>{
     this.log("adding device id:" + device.ID_Category);
     return this.http.post<Device>(this.devicesUrl, device, this.httpOptions)
